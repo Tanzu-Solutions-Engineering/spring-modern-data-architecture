cf965625382844f7753a6389c2607be35f9ec9bd
Demetrious Robinson <demetriousr@demetriousr-a02.vmware.com>
2022-09-26 18:58:42 -0700

    Update Java17 install script

--- deployments/local/scripts/java/install-java-17.sh
+++ deployments/local/scripts/java/install-java-17.sh
@@ -1,11 +1,10 @@
-brew update
 brew install openjdk@17
 
 sudo ln -sfn /usr/local/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
 printf "\n"
 
 echo exporting "JAVA_HOME=\$(/usr/libexec/java_home -v 17)"
-export "JAVA_HOME=$(/usr/libexec/java_home -v 17)"
+export JAVA_HOME=$(/usr/libexec/java_home -v 17)
 
 echo "Java Version 17 Installed"
 printf "\n"
