57aa8da100d47442cc0cf5de585f73f833e0f14a
Gregory Green <gregoryg@gregoryg-a02.vmware.com>
2022-10-07 17:21:24 -0400

    Fixing customer favorites SSE

--- applications/retail-web-app/src/main/java/com/vmware/retail/controller/SaveCustomerFavoritesController.java
+++ applications/retail-web-app/src/main/java/com/vmware/retail/controller/SaveCustomerFavoritesController.java
@@ -2,12 +2,17 @@ package com.vmware.retail.controller;
 
 import com.vmware.retail.domain.CustomerFavorites;
 import com.vmware.retail.repository.CustomerFavoriteRepository;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
@@ -22,7 +27,7 @@ import java.util.concurrent.ThreadFactory;
  */
 @RestController
 @RequestMapping("customer/favorites")
-public class CustomerFavoritesController
+public class SaveCustomerFavoritesController
 {
 
     private final CustomerFavoriteRepository repository;
@@ -31,31 +36,16 @@ public class CustomerFavoritesController
     private final ThreadFactory factory;
 
 
-    private static final Logger logger = LoggerFactory.getLogger(CustomerFavoritesController.class);
+    private static final Logger logger = LoggerFactory.getLogger(SaveCustomerFavoritesController.class);
 
-    public CustomerFavoritesController(CustomerFavoriteRepository repository,
-                                       @Qualifier("webSocketThreadFactory")
+    public SaveCustomerFavoritesController(CustomerFavoriteRepository repository,
+                                           @Qualifier("webSocketThreadFactory")
                                        ThreadFactory factory) {
         this.repository = repository;
         this.factory = factory;
     }
 
-    @GetMapping(value = "favorite/{id}", produces = "text/event-stream")
-    @Async("taskExecutor")
-    public Flux<ServerSentEvent<CustomerFavorites>> findById(@PathVariable String id)
-    {
-        Scheduler scheduler = Schedulers.newParallel(5,factory);
-        return Flux.interval(Duration.ofSeconds(5),scheduler)
 
-                .map(sequence -> {
-                            var favorites = repository.findById(id).orElse(null);
-                            logger.info("favorites: {}",favorites);
-                            var builder = ServerSentEvent
-                                    .builder(favorites);
-                            return builder.build();
-                        }
-                );
-    }
 
     @PostMapping("favorite")
     public void saveCustomerFavorites(@RequestBody CustomerFavorites customerFavorites)
