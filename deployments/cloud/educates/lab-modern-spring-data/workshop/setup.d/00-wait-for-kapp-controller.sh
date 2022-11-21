a0d7a02c1c42dc40bcbbf246a332ea74d386ed65
Gregory Green <gregoryg@vmware.com>
2022-11-21 10:00:22 -0500

    adding Gemfire operator installation

--- deployments/cloud/educates/lab-modern-spring-data/workshop/setup.d/00-wait-for-kapp-controller.sh
+++ deployments/cloud/educates/lab-modern-spring-data/workshop/setup.d/00-wait-for-kapp-controller.sh
@@ -25,3 +25,10 @@ until [ $STATUS -eq 0 ] || $COMMAND || [ $ATTEMPTS -eq 12 ]; do
     STATUS=$?
     ATTEMPTS=$((ATTEMPTS + 1))
 done
+
+
+# install CRD
+kubectl create namespace cert-manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.0/cert-manager.yaml
