cab5ceaeddee97649de45bfca1e73d4099789815
Gregory Green <gregoryg@vmware.com>
2022-11-26 17:20:21 -0500

    Adding templates and cleanup

--- deployments/cloud/educates/lab-modern-spring-data/workshop/setup.d/00-wait-for-kapp-controller.sh
+++ deployments/cloud/educates/lab-modern-spring-data/workshop/setup.d/00-wait-for-kapp-controller.sh
@@ -32,3 +32,9 @@ kubectl create namespace cert-manager
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.0/cert-manager.yaml
+
+
+
+# Template Updates
+
+envsubst < templates/apps/retail-web-app.yml.in > apps/retail-web-app.yml
