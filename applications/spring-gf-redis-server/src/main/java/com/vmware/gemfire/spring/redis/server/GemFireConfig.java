831e83ef9eec9ab60ff99640db4adbcd2bd1f13f
Gregory Green <gregoryg@vmware.com>
2023-04-22 07:09:15 -0400

    Adding Spring config for Redis

--- applications/spring-gf-redis-server/src/main/java/com/vmware/gemfire/spring/redis/server/GemFireConfig.java
+++ applications/spring-gf-redis-server/src/main/java/com/vmware/gemfire/spring/redis/server/GemFireConfig.java
@@ -8,31 +8,61 @@
 package com.vmware.gemfire.spring.redis.server;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.geode.cache.DataPolicy;
 import org.apache.geode.cache.GemFireCache;
+import org.apache.geode.cache.RegionShortcut;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.gemfire.PartitionedRegionFactoryBean;
+import org.springframework.data.gemfire.PeerRegionFactoryBean;
 import org.springframework.data.gemfire.config.annotation.CacheServerApplication;
+import org.springframework.data.gemfire.config.annotation.RegionConfigurer;
 
 
+/**
+ * spring.data.gemfire.locators
+ */
 @Configuration
 @CacheServerApplication
+@Slf4j
 public class GemFireConfig {
 
-//    @Value("${spring.data.gemfire.locators}")
-//    String locators;
+    /**
+     * REGION_NAME = "GF_REDIS_REGION"
+     */
+    private static final String REGION_NAME = "GF_REDIS_REGION";
+
+    /**
+     * PARTITION
+     * PARTITION_REDUNDANT
+     */
+    @Value("${gemfire.redis.region.type:PARTITION}")
+    private String regionTypeName;
 
     @Bean
     PartitionedRegionFactoryBean<String,Object> region(GemFireCache gemFireCache)
     {
+
+        log.info("Create region {} with regionTypeName {}",REGION_NAME,regionTypeName);
+
         var region = new PartitionedRegionFactoryBean<String,Object>();
+
         region.setCache(gemFireCache);
         region.setDataPolicy(DataPolicy.PARTITION);
-        region.setPersistent(false);
-        region.setName("GF_REDIS_REGION");
-        return region;
 
+        region.setPersistent(false);
+        region.setName(REGION_NAME);
+
+        var configurer = new RegionConfigurer() {
+            @Override
+            public void configure(String beanName, PeerRegionFactoryBean<?, ?> bean) {
+                bean.setShortcut(RegionShortcut.valueOf(regionTypeName));
+            }
+        };
+        region.setRegionConfigurers(configurer);
+
+        return region;
     }
 }
