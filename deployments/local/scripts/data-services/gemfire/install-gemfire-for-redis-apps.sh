170a12fe8de80cb445c9972332e573d1de61e249
Gregory Green <gregoryg@vmware.com>
2023-04-24 06:32:02 -0400

    update redis

--- deployments/local/scripts/data-services/gemfire/install-gemfire-for-redis-apps.sh
+++ deployments/local/scripts/data-services/gemfire/install-gemfire-for-redis-apps.sh
@@ -2,16 +2,16 @@
 # This script install GemFire and GemFire for Redis apps addon
 
 mkdir -p ~/spring-modern-data-architecture/data-services/gemfire
-curl -o ~/spring-modern-data-architecture/data-services/gemfire/vmware-gemfire-9.15.0.tgz https://spring-modern-data-architecture-files.s3.us-west-1.amazonaws.com/vmware-gemfire-9.15.0.tgz
+curl -o ~/spring-modern-data-architecture/data-services/gemfire/vmware-gemfire-for-redis-apps-1.1.0.tgz https://cloud-native-data.s3.amazonaws.com/vmware-gemfire-for-redis-apps-1.1.0.tgz
 mkdir -p ~/spring-modern-data-architecture/data-services/gemfire/
 project_dir=$PWD
 cd ~/spring-modern-data-architecture/data-services/gemfire/
-tar -xvf ~/spring-modern-data-architecture/data-services/gemfire/vmware-gemfire-9.15.0.tgz
+tar -xvf ~/spring-modern-data-architecture/data-services/gemfire/vmware-gemfire-for-redis-apps-1.1.0.tgz
 
 
-curl -o ~/spring-modern-data-architecture/data-services/gemfire/gemfire-for-redis-apps-1.0.1.tgz  https://spring-modern-data-architecture-files.s3.us-west-1.amazonaws.com/gemfire-for-redis-apps-1.0.1.tgz
+curl -o ~/spring-modern-data-architecture/data-services/gemfire/vmware-gemfire-for-redis-apps-1.1.0.tgz  https://cloud-native-data.s3.amazonaws.com/vmware-gemfire-for-redis-apps-1.1.0.tgz
 cd ~/spring-modern-data-architecture/data-services/gemfire/
-tar -xvf  ~/spring-modern-data-architecture/data-services/gemfire/gemfire-for-redis-apps-1.0.1.tgz
+tar -xvf  ~/spring-modern-data-architecture/data-services/gemfire/vmware-gemfire-for-redis-apps-1.1.0.tgz
 cd $project_dir
 
 echo ************************************
