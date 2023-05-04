5b70adf0f0f12fce26f01adceedf88b406474b17
Gregory Green <gregoryg@vmware.com>
2023-05-04 10:39:40 -0400

    Redis fix

--- applications/retail-cache-sink-app/src/main/java/com/vmware/retail/analytics/RedisConfig.java
+++ applications/retail-cache-sink-app/src/main/java/com/vmware/retail/analytics/RedisConfig.java
@@ -7,29 +7,20 @@
 
 package com.vmware.retail.analytics;
 
-import com.vmware.retail.domain.CustomerFavorites;
-import com.vmware.retail.domain.Promotion;
 import com.vmware.retail.repository.CustomerFavoriteRepository;
+import nyla.solutions.core.util.Text;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.PatternTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.time.Duration;
-import java.util.function.Consumer;
+import java.util.Calendar;
 
 /**
  * RedisConfig
@@ -40,6 +31,9 @@ import java.util.function.Consumer;
 @EnableRedisRepositories(basePackageClasses = CustomerFavoriteRepository.class)
 public class RedisConfig
 {
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @Bean
     public RedisSerializer redisSerializer()
     {
@@ -50,10 +44,20 @@ public class RedisConfig
     public RedisTemplate redisTemplate(@Qualifier("redisConnectionFactory") RedisConnectionFactory connectionFactory, RedisSerializer redisSerializer)
     {
         var template = new RedisTemplate();
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(redisSerializer);
+        template.setDefaultSerializer(redisSerializer);
         template.setConnectionFactory(connectionFactory);
+
         return template;
+    }
+
+
+    @Bean
+    ApplicationContextAware listener(RedisTemplate<String,String> redisTemplate)
+    {
+        return context -> {
+            redisTemplate.opsForValue().set(applicationName,
+                    Text.formatDate(Calendar.getInstance().getTime()));
+        };
     }
 
 }
