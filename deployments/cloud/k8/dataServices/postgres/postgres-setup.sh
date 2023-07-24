84fe048e929c093ee110750e9258242a4fde7068
Gregory Green <gregoryg@vmware.com>
2023-07-23 20:32:29 -0400

    Adding for Multi-cloud recording

--- deployments/cloud/k8/dataServices/postgres/postgres-setup.sh
+++ deployments/cloud/k8/dataServices/postgres/postgres-setup.sh
@@ -15,22 +15,32 @@ kubectl create secret docker-registry image-pull-secret --docker-server=registry
 
 #docker load -i ~/dataServices/postgres/postgres-for-kubernetes-v1.8.0/images/postgres-instance
 #docker load -i ~/dataServices/postgres/postgres-for-kubernetes-v1.8.0/images/postgres-operator
-docker images "postgres-*"
+#docker images "postgres-*"
 export HELM_EXPERIMENTAL_OCI=1
 helm registry login registry.tanzu.vmware.com --username=$HARBOR_USER --password=$HARBOR_PASSWORD
+
+#helm registry login registry.tanzu.vmware.com --username=$HARBOR_USER --password=$HARBOR_PASSWORD -n sql-system
+
 
 kubectl create secret docker-registry regsecret \
 --docker-server=https://registry.tanzu.vmware.com --docker-username=$HARBOR_USER \
 --docker-password=$HARBOR_PASSWORD
 
-helm pull oci://registry.tanzu.vmware.com/tanzu-sql-postgres/postgres-operator-chart --version v1.9.0 --untar --untardir /tmp
+kubectl create secret docker-registry regsecret \
+--docker-server=https://registry.tanzu.vmware.com --docker-username=$HARBOR_USER \
+--docker-password=$HARBOR_PASSWORD -n sql-system
+
+
+helm pull oci://registry.tanzu.vmware.com/tanzu-sql-postgres/vmware-sql-postgres-operator --version v2.1.0 --untar --untardir /tmp
 
 kubectl create namespace sql-system
-helm install --wait postgres-operator /tmp/postgres-operator/ --namespace=sql-system
+
+#helm install my-postgres-operator /tmp/postgres-operator/  --namespace=sql-system --wait
+helm install --wait postgres-operator /tmp/vmware-sql-postgres-operator/ --namespace=sql-system
 kubectl get crd postgresbackups.sql.tanzu.vmware.com
 
-sleep 30
-kubectl apply -f deployments/cloud/k8/dataServices/postgres/postgres.yml
-sleep 40
-kubectl wait pod -l=app=postgres --for=condition=Ready --timeout=360s
-kubectl wait pod -l=statefulset.kubernetes.io/pod-name=postgres-0 --for=condition=Ready --timeout=360s
+#sleep 30
+#kubectl apply -f deployments/cloud/k8/dataServices/postgres/postgres.yml
+#sleep 40
+#kubectl wait pod -l=app=postgres --for=condition=Ready --timeout=360s
+#kubectl wait pod -l=statefulset.kubernetes.io/pod-name=postgres-0 --for=condition=Ready --timeout=360s
