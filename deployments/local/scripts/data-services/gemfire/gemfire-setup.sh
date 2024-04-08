ee413f089e7ae27e2401ee376de8c2833a757f01
ggreen <gregoryg@vmware.com>
2024-04-08 08:57:18 -0400

    TAS examples

--- deployments/local/scripts/data-services/gemfire/gemfire-setup.sh
+++ deployments/local/scripts/data-services/gemfire/gemfire-setup.sh
@@ -7,11 +7,11 @@
 
 cd $GEMFIRE_HOME/bin
 
-$GEMFIRE_HOME/bin/gfsh -e "start locator --name=locator"
+$GEMFIRE_HOME/bin/gfsh -e "start locator --name=localhost --bind-address=127.0.0.1 --hostname-for-clients=127.0.0.1 --http-service-bind-address=127.0.0.1"
 
 $GEMFIRE_HOME/bin/gfsh -e "connect" - "configure pdx --read-serialized=true --disk-store"
 
-$GEMFIRE_HOME/bin/gfsh -e "start server --name=server --locators=localhost[10334]"
+$GEMFIRE_HOME/bin/gfsh -e "start server --name=server1 --server-bind-address=127.0.0.1 --hostname-for-clients=127.0.0.1 --jmx-manager-hostname-for-clients=127.0.0.1 --bind-address=127.0.0.1 --http-service-bind-address=127.0.0.1 --locators=127.0.0.1[10334]"
 
 $GEMFIRE_HOME/bin/gfsh -e "connect" -e "create region --name=CustomerFavorites --type=PARTITION"
 
